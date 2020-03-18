dcae4ff8a5351300f056fcfea5c23e7a0b46f9ea
zy-yz <1090712762@qq.com>
2020-03-18 15:03:15 +0800

    Initial commit

---
+++ src/main/java/com/zy/jike/JikeApplication.java
@@ -0,0 +1,13 @@
+package com.zy.jike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zy/jike/JikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.jike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
